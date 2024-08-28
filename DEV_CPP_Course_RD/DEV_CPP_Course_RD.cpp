5ee39b1f99502c31328a9c5b9d8303483b48b99c
khristinatsymbala <160432600+khristinatsymbala@users.noreply.github.com>
2024-08-28 15:10:28 +0300

    Add project files.

---
+++ DEV_CPP_Course_RD/DEV_CPP_Course_RD.cpp
@@ -0,0 +1,47 @@
+// DEV_CPP_Course_RD.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    float health{ 0 };
+
+    std::cout << "Enter the hero`s health: " << std::endl;
+    std::cin >> health;
+
+    while (health > 0) {
+
+        float item_distance, item_impact;
+
+        std::cout << "Enter the distance to item : " << std::endl;
+        std::cin >> item_distance;
+
+        std::cout << "Enter item impact (damage/healing): " << std::endl;
+        std::cin >> item_impact;
+
+        if (item_distance <= 100)
+        {
+            float result_impact{ 0 };
+
+            result_impact = item_impact * (1 - item_distance / 100);
+            health += result_impact;
+
+            if (health < 1 && health > 0) {
+                health = 1; 
+            }
+        }
+    }
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
